2c9281fb33a2f7b47168eef258cda4a8fb067df3
Valeriia <108753384+Lekam1991@users.noreply.github.com>
2022-07-10 12:13:58 +0300

    Add files via upload

---
+++ Lessons_2.py
@@ -0,0 +1,24 @@
+x1 = 2 + 2
+print(x1)
+x2 = 9 - 3
+print(x2)
+x3 = 5 * 2
+x4 = 9 ** 7
+print(x4)
+x5 = 8 / 3
+print(x5)
+x6 = 8 // 3
+print(x6)
+x7 = 3 > 1
+x8 = 9 < 10
+x9 = 7 % 2
+print(x9)
+x10 = 4 == 4
+x11 = 4 != 4
+x12 = not True
+print(x12)
+x13 = 2 < 9 and 3 == 3
+print(x13)
+x14 = 2 > 9 or 3 == 3
+print(x14)
+
